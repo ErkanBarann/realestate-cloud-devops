9dfa9f4a0951c8dc058103a85800a92209b76752
ErkanBarann <115561897+ErkanBarann@users.noreply.github.com>
2025-05-26 00:34:35 +0200

    update files

--- Task-1-Solution/Terraform/main.tf
+++ Task-1-Solution/Terraform/main.tf
@@ -17,47 +17,10 @@ resource "aws_instance" "devops_project_instance" {
   count = var.num_of_instance
   iam_instance_profile = "devops-project-profile-techpro"
   key_name = var.ec2_key
-  vpc_security_group_ids = aws_security_group.devops_project_sgr.*.id
+  vpc_security_group_ids = [aws_security_group.devops-project-sgr.id]
   tags = {
     Project = "Devops-Project-Server"
     Name = "${terraform.workspace}_server"
-  }
-}
-
-data "aws_security_group" "existing_sg" {
-  filter {
-    name   = "group-name"
-    values = ["${terraform.workspace}-Security-Group"]
-  }
-
-  filter {
-    name   = "vpc-id"
-    values = ["<your-vpc-id>"]
-  }
-}
-
-resource "aws_security_group" "devops_project_sgr" {
-  count = length(data.aws_security_group.existing_sg.id) == 0 ? 1 : 0
-
-  name        = "${terraform.workspace}-Security-Group"
-  description = "Allow SSH inbound traffic"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "${terraform.workspace}-Security-Group"
   }
 }
 
@@ -77,5 +40,27 @@ data "aws_ami" "al2023" {
   filter {
     name = "name"
     values = ["al2023-ami-2023*"]
+  }
+}
+
+
+resource "aws_security_group" "devops-project-sgr" {
+  name        = "${terraform.workspace}-Security-Group"
+  description = "Devops Project Security Group"
+
+  dynamic "ingress" {
+    for_each = lookup(var.ports, terraform.workspace)
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"] 
+    }
+  }
+  egress {
+    from_port   = 0
+    protocol    = -1
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
